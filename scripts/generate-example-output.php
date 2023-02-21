7ff0a5bc1cf95860a399219fe97636ad68999b8f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-02-21 16:17:08 +0100

    Initial work on tooling to automatically generate example output

---
+++ scripts/generate-example-output.php
@@ -0,0 +1,42 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+foreach (new GlobIterator(__DIR__ . '/../src/**/examples/*Test.php') as $test) {
+    print $test->getRealPath() . PHP_EOL;
+
+    $bootstrap = dirname($test->getRealPath()) . '/src/autoload.php';
+    $options   = '';
+
+    $command = __DIR__ . '/../tools/phpunit ';
+
+    if (file_exists($bootstrap)) {
+        $command .= '--bootstrap ' . $bootstrap . ' ';
+    }
+
+    $output = shell_exec($command . $options . $test);
+
+    if (str_contains($output, ', Incomplete:')) {
+        $options .= '--display-incomplete ';
+
+        $output = shell_exec($command . $options . $test);
+    }
+
+    if (str_contains($output, ', Skipped:')) {
+        $options .= '--display-skipped ';
+
+        $output = shell_exec($command . $options . $test);
+    }
+
+    file_put_contents(
+        $test . '.out',
+        './tools/phpunit ' . $options . 'tests/' . $test->getBasename() . PHP_EOL .
+        str_replace(
+            [
+                dirname($test->getRealPath()),
+            ],
+            [
+                '/path/to/tests',
+            ],
+            $output
+        )
+    );
+}
