b807698bcf459b2b9e9863f40529248eca5f5f6b
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-02-21 16:30:26 +0100

    Run tests with --no-configuration and --do-not-cache-result options

--- scripts/generate-example-output.php
+++ scripts/generate-example-output.php
@@ -3,27 +3,28 @@
 foreach (new GlobIterator(__DIR__ . '/../src/**/examples/*Test.php') as $test) {
     print $test->getRealPath() . PHP_EOL;
 
-    $bootstrap = dirname($test->getRealPath()) . '/src/autoload.php';
-    $options   = '';
+    $bootstrap     = dirname($test->getRealPath()) . '/src/autoload.php';
+    $hiddenOptions = '--no-configuration --do-not-cache-result ';
+    $options       = '';
 
     $command = __DIR__ . '/../tools/phpunit ';
 
     if (file_exists($bootstrap)) {
-        $command .= '--bootstrap ' . $bootstrap . ' ';
+        $hiddenOptions .= '--bootstrap ' . $bootstrap . ' ';
     }
 
-    $output = shell_exec($command . $options . $test);
+    $output = shell_exec($command . $hiddenOptions . $options . $test);
 
     if (str_contains($output, ', Incomplete:')) {
         $options .= '--display-incomplete ';
 
-        $output = shell_exec($command . $options . $test);
+        $output = shell_exec($command . $hiddenOptions . $options . $test);
     }
 
     if (str_contains($output, ', Skipped:')) {
         $options .= '--display-skipped ';
 
-        $output = shell_exec($command . $options . $test);
+        $output = shell_exec($command . $hiddenOptions . $options . $test);
     }
 
     file_put_contents(
