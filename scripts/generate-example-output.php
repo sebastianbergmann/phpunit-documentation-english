12a010d0c2c2f9f8668f20b600e6e531dd320c5f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-02-22 12:44:25 +0100

    Avoid redundant work

--- scripts/generate-example-output.php
+++ scripts/generate-example-output.php
@@ -1,13 +1,24 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
-foreach (new GlobIterator(__DIR__ . '/../src/examples/**/*Test.php') as $test) {
-    print $test->getRealPath() . PHP_EOL;
+$phpunit       = $command = __DIR__ . '/../tools/phpunit';
+$version       = hash('sha256', trim(PHP_VERSION . shell_exec($phpunit . ' --version')));
+$processed     = processed_read();
+$rootDirectory = realpath(__DIR__ . '/../src/examples') . '/';
 
+foreach (new GlobIterator(__DIR__ . '/../src/examples/**/*Test.php') as $test) {
+    $currentFile     = str_replace($rootDirectory, '', $test->getRealPath());
+    $currentFileHash = $version . hash_file('sha256', $test->getRealPath());
+
+    if (isset($processed[$currentFile]) && $processed[$currentFile] === $currentFileHash) {
+        print '[skipped  ] ' . $currentFile . PHP_EOL;
+
+        continue;
+    }
+
+    $command       = $phpunit . ' ';
     $bootstrap     = dirname($test->getRealPath()) . '/src/autoload.php';
     $hiddenOptions = '--no-configuration --do-not-cache-result ';
     $options       = '';
-
-    $command = __DIR__ . '/../tools/phpunit ';
 
     if (file_exists($bootstrap)) {
         $hiddenOptions .= '--bootstrap ' . $bootstrap . ' ';
@@ -40,4 +51,36 @@ foreach (new GlobIterator(__DIR__ . '/../src/examples/**/*Test.php') as $test) {
             $output
         )
     );
+
+    $processed[$currentFile] = $currentFileHash;
+
+    print '[processed] ' . $currentFile . PHP_EOL;
+}
+
+processed_write($processed);
+
+function processed_write(array $processed): void
+{
+    file_put_contents(__DIR__ . '/processed.json', json_encode($processed, JSON_PRETTY_PRINT));
+}
+
+function processed_read(): array
+{
+    $filename = __DIR__ . '/processed.json';
+
+    if (!file_exists($filename)) {
+        return [];
+    }
+
+    $json = file_get_contents($filename);
+
+    if (!$json) {
+        return [];
+    }
+
+    try {
+        return json_decode($json, true, flags: JSON_THROW_ON_ERROR);
+    } catch (JsonException) {
+        return [];
+    }
 }
